e88721f1c6fc1592168d1dceabfc1997a8b76d31
mahbubrahman1 <mahbuburrahman3@yahoo.com>
2022-10-24 11:22:24 +0600

    added more core funtionality

--- Main/urls.py
+++ Main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import TaskList, CreateTask, TaskDetail, EditTask, DeleteTask, UserSignin, LogoutView
+from .views import TaskList, CreateTask, TaskDetail, EditTask, DeleteTask, UserSignin, LogoutView, UserSignup
 
 
 urlpatterns = [
@@ -11,4 +11,5 @@ urlpatterns = [
     path('delete/<str:pk>/', DeleteTask.as_view(), name='delete'),
     path('signin/', UserSignin.as_view(), name='signin'),
     path('signout/', LogoutView.as_view(next_page='signin'), name='signout'),
+    path('signup/', UserSignup.as_view(), name='signup'),
 ]
--- Main/views.py
+++ Main/views.py
@@ -1,7 +1,10 @@
-from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView, FormView
 from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.shortcuts import redirect
 
 from .models import Task
 
@@ -16,6 +19,12 @@ class TaskList(LoginRequiredMixin, ListView):
         context = super().get_context_data(**kwargs)
         context['tasks'] = context['tasks'].filter(user=self.request.user)
         context['count'] = context['tasks'].filter(complete=False).count()
+
+        # search 
+        search_input = self.request.GET.get('search-area') or ''
+        if search_input:
+            context['tasks'] = context['tasks'].filter(title__startswith=search_input)
+        context['search_input'] = search_input
         return context
 
 
@@ -62,3 +71,22 @@ class UserSignin(LoginView):
 
     def get_success_url(self):
         return reverse_lazy('task_list')
+
+
+# user signup or registration
+class UserSignup(FormView):
+    template_name = 'Main/signup.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('task_list')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(UserSignup, self).form_valid(form)
+
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('tasks')
+        return super(UserSignup, self).get(*args, **kwargs)
